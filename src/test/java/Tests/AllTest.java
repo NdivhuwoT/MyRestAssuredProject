4934fce7db3399fbac119d7cdeb6e3d2c0093274
Ndivhuwo <tshiedzan@gmail.com>
2026-03-25 21:43:38 +0200

    Added UserLogin Method

--- src/test/java/Tests/AllTest.java
+++ src/test/java/Tests/AllTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 public class AllTest {
 
     static String RegisteredEmail;
+    static String RegisteredPassword = "P@sswordreg";
 
     @Test
     public void adminLoginTest() {
@@ -19,7 +20,7 @@ public class AllTest {
     @Test(priority = 1)
     public void userRegistrationTest() {
         RegisteredEmail = Faker.instance().internet().emailAddress();
-        ApiRequestBuilder.userRegisterResponse("RegName","RegLastName", RegisteredEmail, "P@sswordreg","P@sswordreg", "5328c91e-fc40-11f0-8e00-5000e6331276")
+        ApiRequestBuilder.userRegisterResponse("RegName","RegLastName", RegisteredEmail, RegisteredPassword, RegisteredPassword, "5328c91e-fc40-11f0-8e00-5000e6331276")
                 .then()
                 .log().all()
                 .assertThat()
@@ -29,6 +30,15 @@ public class AllTest {
     @Test(priority = 2)
     public void userApprovalTest() {
         ApiRequestBuilder.userApprovalResponse()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test(dependsOnMethods = "userApprovalTest")
+    public void userLoginTest() {
+        ApiRequestBuilder.userLoginResponse(RegisteredEmail, RegisteredPassword)
                 .then()
                 .log().all()
                 .assertThat()
